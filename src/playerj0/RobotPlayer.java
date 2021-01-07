4b351e8ab9a45b49b06df764a04f9234d2fede8b
joachimkennedy <36801301+joachimkennedy@users.noreply.github.com>
2021-01-07 13:28:37 -0600

    influence bug

--- src/playerj0/RobotPlayer.java
+++ src/playerj0/RobotPlayer.java
@@ -37,14 +37,14 @@ public strictfp class RobotPlayer {
 
         turnCount = 0;
 
-        System.out.println("I'm a " + rc.getType() + " and I just got created!");
+        //System.out.println("I'm a " + rc.getType() + " and I just got created!");
         while (true) {
             turnCount += 1;
             // Try/catch blocks stop unhandled exceptions, which cause your robot to freeze
             try {
                 // Here, we've separated the controls into a different method for each RobotType.
                 // You may rewrite this into your own control structure if you wish.
-                System.out.println("I'm a " + rc.getType() + "! Location " + rc.getLocation());
+                //System.out.println("I'm a " + rc.getType() + "! Location " + rc.getLocation());
                 switch (rc.getType()) {
                     case ENLIGHTENMENT_CENTER: runEnlightenmentCenter(); break;
                     case POLITICIAN:           runPolitician();          break;
@@ -66,7 +66,7 @@ public strictfp class RobotPlayer {
         RobotType toBuild = randomSpawnableRobotType();
         int influence = 50;
 
-        
+        System.out.println(rc.getInfluence());
         if (rc.getRoundNum()%50 == 0 && rc.getRoundNum() < 1000) {
             toBuild = RobotType.SLANDERER;
             influence = rc.getInfluence() - rc.getInfluence()%20;
@@ -94,13 +94,13 @@ public strictfp class RobotPlayer {
         int actionRadius = rc.getType().actionRadiusSquared;
         RobotInfo[] attackable = rc.senseNearbyRobots(actionRadius, enemy);
         if (attackable.length != 0 && rc.canEmpower(actionRadius)) {
-            System.out.println("empowering...");
+            //System.out.println("empowering...");
             rc.empower(actionRadius);
-            System.out.println("empowered");
+            //System.out.println("empowered");
             return;
         }
         if (tryMove(randomDirection()))
-            System.out.println("I moved!");
+            ;
     }
 
     static void runSlanderer() throws GameActionException {
@@ -118,14 +118,14 @@ public strictfp class RobotPlayer {
             if (robot.type.canBeExposed()) {
                 // It's a slanderer... go get them!
                 if (rc.canExpose(robot.location)) {
-                    System.out.println("e x p o s e d");
+                    //System.out.println("e x p o s e d");
                     rc.expose(robot.location);
                     return;
                 }
             }
         }
         if (tryMove(randomDirection()))
-            System.out.println("I moved!");
+            ;
     }
 
     /**
@@ -154,7 +154,7 @@ public strictfp class RobotPlayer {
      * @throws GameActionException
      */
     static boolean tryMove(Direction dir) throws GameActionException {
-        System.out.println("I am trying to move " + dir + "; " + rc.isReady() + " " + rc.getCooldownTurns() + " " + rc.canMove(dir));
+        //System.out.println("I am trying to move " + dir + "; " + rc.isReady() + " " + rc.getCooldownTurns() + " " + rc.canMove(dir));
         if (rc.canMove(dir)) {
             rc.move(dir);
             return true;
