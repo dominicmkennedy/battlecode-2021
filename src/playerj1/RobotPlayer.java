cc4ec042fd8d16384ed2fcc18e16fbebe7d2b513
joachimkennedy <36801301+joachimkennedy@users.noreply.github.com>
2021-01-08 14:15:21 -0600

    scouting

--- src/playerj1/RobotPlayer.java
+++ src/playerj1/RobotPlayer.java
@@ -1,6 +1,7 @@
 package playerj1;
 import battlecode.common.*;
 import java.lang.Math;
+import java.util.*;
 
 public strictfp class RobotPlayer {
     static RobotController rc;
@@ -66,60 +67,110 @@ public strictfp class RobotPlayer {
         RobotType toBuild = randomSpawnableRobotType();
         int influence = 50;
         int bidAmt = 1;
-
-        if (rc.getRoundNum() == 0) {
-            toBuild = RobotType.SLANDERER;
-            influence = 140;
-        }
-
-        for (Direction dir : directions) {
-            if (rc.canBuildRobot(toBuild, dir, influence)) {
-                rc.buildRobot(toBuild, dir, influence);
-            } else {
-                break;
-            }
-        }
-
-        if (rc.canBid(bidAmt)) {
-            rc.bid(bidAmt);
-        }
-        Clock.yield();
-
+        int flag = 0;
+        int roundNum = rc.getRoundNum();
+        Direction direction = Direction.NORTH;
+        Hashtable<Integer, Integer> idToFlag = new Hashtable<Integer, Integer>();
         int i = 0;
-        if (rc.getRoundNum() <= 50) {
-            toBuild = RobotType.POLITICIAN;
-            influence = 1;
-            while (i < 24) {
-                if (rc.canBuildRobot(toBuild, directions[i%8], influence)) {
+        Boolean canBuild = false;
+        ArrayList<Integer> toRemove = new ArrayList<Integer>();
+
+        rc.setFlag(flag);
+
+        while (true) {
+            roundNum = rc.getRoundNum();
+            // Check whether flags have changed
+            for (int id : idToFlag.keySet()) {
+                if (!rc.canGetFlag(id)) {
+                    toRemove.add(id);
+                } else if (idToFlag.containsKey(id)) {
+                    if (rc.getFlag(id) != idToFlag.get(id)) {
+                        if (rc.canSetFlag(rc.getFlag(id))) {
+                            rc.setFlag(rc.getFlag(id));  // In future iteration, make it so that it stores all new flags and deals with them
+                            break;
+                        }
+                        idToFlag.put(id,rc.getFlag(id));
+                    }
+                }
+            }
+
+            // Remove unreachable ids from idToFlag
+            for (int j = 0; j < toRemove.size(); j++) {
+                idToFlag.remove(toRemove.get(j));
+            }
+
+            // Decide toBuild and influence (and direction) based on round number
+            if (roundNum%50==0) {
+                toBuild = RobotType.SLANDERER;
+                influence = rc.getInfluence() - rc.getInfluence()%20;
+                for (Direction dir : directions) {
+                    if (rc.canBuildRobot(toBuild, dir, influence)) {
+                        rc.buildRobot(toBuild, dir, influence);
+                        break;
+                    }
+                }
+            } else if (i < 24) {
+                toBuild = RobotType.POLITICIAN;
+                influence = 1;
+                if (rc.canBuildRobot(toBuild,directions[i%8], influence)) {
                     rc.buildRobot(toBuild, directions[i%8], influence);
                     i++;
                 }
-                if (rc.canBid(bidAmt)) {
-                    rc.bid(bidAmt);
+            } else if (roundNum%10 == 0) {
+                toBuild = RobotType.MUCKRAKER;
+                influence = 2;
+                for (Direction dir : directions) {
+                    if (rc.canBuildRobot(toBuild, dir, influence)) {
+                        rc.buildRobot(toBuild, dir, influence);
+                        break;
+                    }
                 }
-                Clock.yield();
-            }
-        }
-
-        if (rc.getRoundNum()%50 == 0 && rc.getRoundNum() < 1000) {
-            toBuild = RobotType.SLANDERER;
-            influence = rc.getInfluence() - rc.getInfluence()%20;
-        }
-
-        
-        for (Direction dir : directions) {
-            if (rc.canBuildRobot(toBuild, dir, influence)) {
-                rc.buildRobot(toBuild, dir, influence);
+            } else if (rc.getFlag(rc.getID()) != 0 && roundNum < 2000) {
+                toBuild = RobotType.POLITICIAN;
+                influence = 100;
+                for (Direction dir : directions) {
+                    if (rc.canBuildRobot(toBuild, dir, influence)) {
+                        rc.buildRobot(toBuild, dir, influence);
+                        break;
+                    }
+                }
             } else {
-                break;
+                toBuild = randomSpawnableRobotType();
+                influence = 20;
+                for (Direction dir : directions) {
+                    if (rc.canBuildRobot(toBuild, dir, influence)) {
+                        rc.buildRobot(toBuild, dir, influence);
+                        break;
+                    }
+                }
             }
-        }
 
-        bidAmt = (int)Math.ceil(rc.getInfluence()/(3000-rc.getRoundNum()) + 1);
-        if (rc.canBid(bidAmt)) {
-            rc.bid(bidAmt);
-        } else if (rc.canBid(1)) {
-            rc.bid(1);
+
+            // Add new bots to the id roster
+            for (RobotInfo bot : rc.senseNearbyRobots(8, rc.getTeam())) {
+                if (!idToFlag.containsKey(bot.getID()) && bot.getType() == RobotType.POLITICIAN) {
+                    idToFlag.put(bot.getID(),rc.getFlag(bot.getID()));
+                }
+            }
+
+
+            // Generic bid code
+            if (roundNum > 1500) {
+                bidAmt = (int)Math.ceil(rc.getInfluence()/(3000-roundNum) + 1);
+            } else if (rc.getInfluence() < 100) {
+                bidAmt = Math.max(6,rc.getInfluence());
+            } else {
+                bidAmt = (int)(rc.getInfluence()/10);
+            }
+
+
+            if (rc.canBid(bidAmt)) {
+                rc.bid(bidAmt);
+            } else if (rc.canBid(1)) {
+                rc.bid(1);
+            }
+            // System.out.println("Bytecode used: " + Clock.getBytecodeNum());
+            Clock.yield();
         }
 
     }
@@ -142,10 +193,28 @@ public strictfp class RobotPlayer {
         MapLocation destination = new MapLocation(30064, 30064); // Change this to a random location within some radius of the starting location
         int id = rc.getID();
         Team enemy = rc.getTeam().opponent();
+        RobotInfo[] nearbyBots = rc.senseNearbyRobots();
+        MapLocation ecLoc = rc.getLocation();
+        int ecID = 0;
+        int ecFlag = 0;
 
+        // Get info from home EC base
+        for (RobotInfo bot : nearbyBots) {
+            if (bot.getType() == RobotType.ENLIGHTENMENT_CENTER && bot.getTeam() == rc.getTeam()) {
+                ecLoc = bot.getLocation();
+                ecID = bot.getID();
+                ecFlag = rc.getFlag(ecID);
+            }
+        }
 
-        if (rc.getRoundNum() <= 100) {
+        // Differentiate class
+        if (ecFlag != 0) {
+            destination = interpretFlag(ecFlag, ecLoc);
+            runAttacker(destination);
+            return;
+        } else if (rc.getRoundNum() <= 100) {
             runScout();
+            return;
         }
 
         while (true) {
@@ -155,16 +224,13 @@ public strictfp class RobotPlayer {
             Direction direction = location.directionTo(destination);
 
             if (attackable.length != 0 && rc.canEmpower(actionRadius)) {
-                //System.out.println("empowering...");
                 rc.empower(actionRadius);
-                //System.out.println("empowered");
                 return;
-            }
-            if (tryMove(direction)) {
-                ;
-            } else {
-                tryMove(randomDirection());
-            }
+            } 
+
+            tryMove(randomDirection());
+
+
             Clock.yield();
         }
     }
@@ -176,6 +242,7 @@ public strictfp class RobotPlayer {
         MapLocation flare = rc.getLocation();
         int flag = 0;
 
+        rc.setFlag(flag);
         for (RobotInfo bot : nearbyBots) {
             if (bot.getType() == RobotType.ENLIGHTENMENT_CENTER) {
                 ecLoc = bot.getLocation();
@@ -210,6 +277,44 @@ public strictfp class RobotPlayer {
             Clock.yield();
         }
         
+    }
+
+    static void runAttacker(MapLocation destination) throws GameActionException {
+        MapLocation location = rc.getLocation();
+        int actionRadius = rc.getType().actionRadiusSquared;
+        Team enemy = rc.getTeam().opponent();
+        RobotInfo[] attackable = rc.senseNearbyRobots(actionRadius, enemy);
+        RobotInfo[] convertible = rc.senseNearbyRobots(actionRadius, Team.NEUTRAL);
+        Direction direction = location.directionTo(destination);
+        Boolean nearEC = false;
+
+        while (true) {
+            location = rc.getLocation();
+            direction = location.directionTo(destination);
+            attackable = rc.senseNearbyRobots(actionRadius, enemy);
+            convertible = rc.senseNearbyRobots(actionRadius, Team.NEUTRAL);
+            for (RobotInfo bot : attackable) {
+                if (bot.getType() == RobotType.ENLIGHTENMENT_CENTER) {
+                    nearEC = true;
+                    break;
+                }
+            }
+            if (rc.canEmpower(actionRadius)) {
+                if (nearEC || convertible.length != 0) {
+                    rc.empower(actionRadius);
+                    return;
+                }
+            }
+
+            if (tryMove(direction)) {
+                ;
+            } else if (tryMove(direction.rotateLeft())) {
+                ;
+            } else if (tryMove(direction.rotateRight())) {
+                ;
+            }
+            Clock.yield();
+        }
     }
 
     static void runSlanderer() throws GameActionException {
@@ -268,5 +373,28 @@ public strictfp class RobotPlayer {
             rc.move(dir);
             return true;
         } else return false;
+    }
+
+    /**
+     * Interpret information from a flag
+     *
+     * @return a the location indicated by the flag (for now)
+     */
+    static MapLocation interpretFlag(int flag, MapLocation ecLoc) {
+        int xCoord = 0;
+        int yCoord = 0;
+
+        yCoord = flag % 100;
+        xCoord = ((int) (flag/100)) % 100;
+        if (flag < 100000) {
+            xCoord = -xCoord;
+            if (flag < 10000) {
+                yCoord = -yCoord;
+            }
+        } else if (flag - 100000 < 10000) {
+            yCoord = -yCoord;
+        }
+
+        return new MapLocation(ecLoc.x + xCoord, ecLoc.y + yCoord);
     }
 }
